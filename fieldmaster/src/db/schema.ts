af76b353eff4a92bc3a3c0d3f7e10f19e39aaf4e
KulmerKlara <klara.kulmer@outlook.com>
2025-11-09 08:11:07 +0100

    tool table in db and create-, get- tool from db

--- fieldmaster/src/db/schema.ts
+++ fieldmaster/src/db/schema.ts
@@ -1,8 +1,16 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, boolean as PgBoolean} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
     age: integer().notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
+});
+
+// Tools Table
+export const toolsTable = pgTable("tools", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    category: varchar({ length: 255 }).notNull(), 
+    available: PgBoolean().notNull(), 
 });
